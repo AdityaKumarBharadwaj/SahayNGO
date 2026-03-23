1990982aaf26ea87ab7c81d74a5980444ccf2dbe
Aditya Kumar <kumarsinghaditya240@gmail.com>
2026-03-24 00:15:38 +0530

    add protected routes

--- Backend/routes/authRoutes.js
+++ Backend/routes/authRoutes.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const {register, login, getMe} = require('../controllers/authController');
+const {protect} = require('../middlewares/authMiddlewares');
 
-// Register route - public
+// public routes
 router.post('/register', register);
-
-// Login route - public
 router.post('/login', login);
+
+// protected route
+router.get('/me', protect, getMe);
 
 module.exports = router;
